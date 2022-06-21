d6c315270a2a17228f993c9fdb199db1c2f3ec7c
Chydfile <48090062+Chydfile@users.noreply.github.com>
2022-06-21 17:06:41 +0300

    Update server.py

--- server.py
+++ server.py
@@ -14,21 +14,21 @@ st.image("https://i.postimg.cc/yN20YX4F/Stories.png", use_column_width=True)
 #keywords = ['Princess stuck in tower', 'Dragon and birds','Little boy, who disobey parent','Sun day','Little plant', 'Dinosaur and men']
 keywords = ['Flowers and bees']
 
-def GPT_Completion(texts):
-    ## Call the API key under your account (in a secure way)
-    openai.api_key = 'sk-frXvTud0CBEIqZ33z33LT3BlbkFJJTUQammxhA4ppfP8Br'
-    response = openai.Completion.create(
-    engine="text-davinci-002",
-    prompt =  texts,
-    temperature = 0.8,
-    top_p = 1,
-    max_tokens = 2000,
-    frequency_penalty = 0,
-    presence_penalty = 0,
-    n = 1
-    )
-    print(response.choices[0].text)
-    return response.choices[0].text
+# #def GPT_Completion(texts):
+#     ## Call the API key under your account (in a secure way)
+#     openai.api_key = 'sk-frXvTud0CBEIqZ33z33LT3BlbkFJJTUQammxhA4ppfP8Br'
+#     response = openai.Completion.create(
+#     engine="text-davinci-002",
+#     prompt =  texts,
+#     temperature = 0.8,
+#     top_p = 1,
+#     max_tokens = 2000,
+#     frequency_penalty = 0,
+#     presence_penalty = 0,
+#     n = 1
+#     )
+#     print(response.choices[0].text)
+#     return response.choices[0].text
 
 # add_selectbox = st.sidebar.selectbox(
 #     "*Название города",
@@ -50,10 +50,10 @@ try:
 
     if submit:
         recipe = 'Tell fairy tail about {}.'.format(command)
-        responce = GPT_Completion(recipe)
-        st.text_area('Fairy tail about {}:'.format(command), responce)
+        #responce = GPT_Completion(recipe)
+        #st.text_area('Fairy tail about {}:'.format(command), responce)
         language = 'en'
-        myobj = gTTS(text=responce, lang=language, slow=False)
+        myobj = gTTS(text=recipe, lang=language, slow=False)
         myobj.save("welcome.mp3")
         audio_file = open('welcome.mp3', 'rb')
         audio_bytes = audio_file.read()
