1520a0d7905791d8dc40bb23b80ace4637d31ae2
Chydfile <48090062+Chydfile@users.noreply.github.com>
2022-06-21 14:18:02 +0300

    Update server.py

--- server.py
+++ server.py
@@ -2,20 +2,20 @@ import streamlit as st
 import sys
 import numpy as np
 import pandas as pd
-st.title('Предсказание арендной стоимости жилья для арендодателя на основании заданных критериев')
+st.title('Fairytail generation')
 
 
-add_selectbox = st.sidebar.selectbox(
-    "*Название города",
-    ("Лондон",)
-)
-add_selectbox = st.sidebar.selectbox(
-    "*Язык",
-    ("Русский",)
-)
-st.sidebar.write('*В демонстрационной версии доступен только один город - Лондон и родной язык разработчика.')
-st.sidebar.write('Модель построена с использованием данных c Airbnb, оценивались данные о 40к объектов в Лондоне.')
-st.sidebar.write('В демонстационной версии использованы не все доступные признаки. Инструмент, выбранный для предсказания - Сatboost.')
+# add_selectbox = st.sidebar.selectbox(
+#     "*Название города",
+#     ("Лондон",)
+# )
+# add_selectbox = st.sidebar.selectbox(
+#     "*Язык",
+#     ("Русский",)
+# )
+# st.sidebar.write('*В демонстрационной версии доступен только один город - Лондон и родной язык разработчика.')
+# st.sidebar.write('Модель построена с использованием данных c Airbnb, оценивались данные о 40к объектов в Лондоне.')
+# st.sidebar.write('В демонстационной версии использованы не все доступные признаки. Инструмент, выбранный для предсказания - Сatboost.')
 district_type = st.selectbox(
 "Choose your hero",
 ('Apartment', 'House', 'Condominium', 'Boat', 'Hostel'))
