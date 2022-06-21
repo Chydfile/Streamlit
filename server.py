882cc93e92504870bb68662cc6a52dc5482f0318
Chydfile <48090062+Chydfile@users.noreply.github.com>
2022-06-21 14:16:35 +0300

    Update server.py

--- server.py
+++ server.py
@@ -16,3 +16,7 @@ add_selectbox = st.sidebar.selectbox(
 st.sidebar.write('*В демонстрационной версии доступен только один город - Лондон и родной язык разработчика.')
 st.sidebar.write('Модель построена с использованием данных c Airbnb, оценивались данные о 40к объектов в Лондоне.')
 st.sidebar.write('В демонстационной версии использованы не все доступные признаки. Инструмент, выбранный для предсказания - Сatboost.')
+district_type = st.selectbox(
+"Choose your hero",
+('Apartment', 'House', 'Condominium', 'Boat', 'Hostel'))
+
