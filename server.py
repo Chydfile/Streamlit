7b30f99ef5bf4bfc986c62f2d5896dcbed96d790
Chydfile <48090062+Chydfile@users.noreply.github.com>
2022-06-21 18:18:40 +0300

    Update server.py

--- server.py
+++ server.py
@@ -14,33 +14,29 @@ st.image("https://i.postimg.cc/yN20YX4F/Stories.png", use_column_width=True)
 #keywords = ['Princess stuck in tower', 'Dragon and birds','Little boy, who disobey parent','Sun day','Little plant', 'Dinosaur and men']
 keywords = ['Flowers and bees']
 
-# #def GPT_Completion(texts):
-#     ## Call the API key under your account (in a secure way)
-#     openai.api_key = 'sk-frXvTud0CBEIqZ33z33LT3BlbkFJJTUQammxhA4ppfP8Br'
-#     response = openai.Completion.create(
-#     engine="text-davinci-002",
-#     prompt =  texts,
-#     temperature = 0.8,
-#     top_p = 1,
-#     max_tokens = 2000,
-#     frequency_penalty = 0,
-#     presence_penalty = 0,
-#     n = 1
-#     )
-#     print(response.choices[0].text)
-#     return response.choices[0].text
+def GPT_Completion(texts, key):
+    ## Call the API key under your account (in a secure way)
+    openai.api_key = key
+    response = openai.Completion.create(
+    engine="text-davinci-002",
+    prompt =  texts,
+    temperature = 0.8,
+    top_p = 1,
+    max_tokens = 2000,
+    frequency_penalty = 0,
+    presence_penalty = 0,
+    n = 1
+    )
+    print(response.choices[0].text)
+    return response.choices[0].text
 
-# add_selectbox = st.sidebar.selectbox(
-#     "*Название города",
-#     ("Лондон",)
-# )
-# add_selectbox = st.sidebar.selectbox(
-#     "*Язык",
-#     ("Русский",)
-# )
-# st.sidebar.write('*В демонстрационной версии доступен только один город - Лондон и родной язык разработчика.')
-# st.sidebar.write('Модель построена с использованием данных c Airbnb, оценивались данные о 40к объектов в Лондоне.')
-# st.sidebar.write('В демонстационной версии использованы не все доступные признаки. Инструмент, выбранный для предсказания - Сatboost.')
+try:
+    form_0 = st.form(key='my-form0')
+    key = form_0.text_input('Key for OpenAI')
+    submit_0 = form_0.form_submit_button('Submit')
+
+except Exception as e:
+    st.success(f'Need key to proceed')
 
 try:
     form_1 = st.form(key='my-form1')
@@ -50,17 +46,18 @@ try:
 
     if submit:
         recipe = 'Tell fairy tail about {}.'.format(command)
-        #responce = GPT_Completion(recipe)
-        #st.text_area('Fairy tail about {}:'.format(command), responce)
+        responce = GPT_Completion(recipe, key)
+        st.text_area('Fairy tail about {}:'.format(command), responce)
         language = 'en'
-        myobj = gTTS(text=recipe, lang=language, slow=False)
+        myobj = gTTS(text=responce, lang=language, slow=False)
         myobj.save("welcome.mp3")
         audio_file = open('welcome.mp3', 'rb')
         audio_bytes = audio_file.read()
         st.audio(audio_bytes)
-        
+            
 except Exception as e:
-    st.success(f'Something Went Wrong!😁 {e}')
+    st.success(f'Something Went Wrong! {e}')
+
 
 #print(f'Audio content written to file "{outfile}"')
 
