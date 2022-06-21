b00665fc21ae959e0a2121f8d29e67d3ca83ba0c
Chydfile <48090062+Chydfile@users.noreply.github.com>
2022-06-21 14:28:34 +0300

    Update server.py

--- server.py
+++ server.py
@@ -2,6 +2,9 @@ import streamlit as st
 import sys
 import numpy as np
 import pandas as pd
+!pip install openai
+!git clone https://github.com/AI-Fairytales/GPT3
+!wget https://raw.githubusercontent.com/AI-Fairytales/GPT-3/master/merged_clean2.txt
 st.title('Fairytail generation')
 st.image("https://i.postimg.cc/yN20YX4F/Stories.png", use_column_width=True)
 
@@ -19,5 +22,5 @@ st.image("https://i.postimg.cc/yN20YX4F/Stories.png", use_column_width=True)
 # st.sidebar.write('В демонстационной версии использованы не все доступные признаки. Инструмент, выбранный для предсказания - Сatboost.')
 district_type = st.selectbox(
 "Choose your hero",
-('Dragon', 'Princess', 'Knight', 'Dog', 'King'))
+('Knight','Princess', 'Dragon', 'Dog', 'King'))
 
